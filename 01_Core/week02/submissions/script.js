b46999b6456d80369014ce600e74c0ab06596ee6
zdiaz2826 <56599905+zdiaz2826@users.noreply.github.com>
2019-10-20 12:31:39 -0400

    update final changes

--- 01_Core/week02/submissions/script.js
+++ 01_Core/week02/submissions/script.js
@@ -1,12 +1,12 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("myDropdown").classList.toggle("show"); //points to the dropdwon menu's class adds a toggle method to show the drop down menu.
   }
   
   // Close the dropdown if the user clicks outside of it
   window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
+    if (!e.target.matches('.dropbtn')) {                  // if the user is not clicking on the target element then removes the class value "show"
     var myDropdown = document.getElementById("myDropdown");
       if (myDropdown.classList.contains('show')) {
         myDropdown.classList.remove('show');
@@ -14,31 +14,35 @@ function myFunction() {
     }
   }
   // create a alphabetically-sorted list by last name.
-// function getNames(){
-//     var names = document.getElementById("myText").value;
-//     var namesArr = names.split(', ');
-//     var sortedNames = namesArr.sort();
-//     var fullHTML = sortedNames.join();
-//     document.getElementById("list").innerHTML = fullHTML;
-
-// }
+function getNames(){
+    var names = document.getElementById("myText").value; //points to the value of the textArea.
+    var namesArr = names.split(',');   //Splits the names with a comma 
+    var sortedNames = namesArr.sort(); //sorts the names in acending order
+    var fullHTML = sortedNames.join(' ') // joins  the names with a space
+    document.getElementById("list").innerHTML = fullHTML;   //replaces the content in the empty div with the new array of names.
+}
 
 // string of full names
 
 var names = ["Ziara Diaz", "Nick Strumpf", "Malcolm Taylor", "Chris Smith"];
 
-var sortedNames = names.sort((a,b) => {
- var aSplit = a.split(' ');
- var aLastName = aSplit[1].toLowerCase();
- var bSplit = b.split(' ');
- var bLastName = bSplit[1].toLowerCase();
- if(aLastName < bLastName) return -1;
- if(aLastName > bLastName) return 1;
+var allNames = names.sort((a,b) => {  //Compare function that takes in the name and last name and compares them.
+ var aSplit = a.split(' ');        // splits the first parameter with a space    
+ var aLastName = aSplit[1].toLowerCase();   // looks at the second value of the a paramenter (the last name)
+ var bSplit = b.split(' ');                  // splits the second parameter ('Nick Strumpf')
+ var bLastName = bSplit[1].toLowerCase();  // looks at the second value of the parameter ('strumpf')
+ if(aLastName < bLastName) return -1;    // if the first last name is less than the second last name puts it first.
+ if(aLastName > bLastName) return 1;  // if the first last name is greater than it will put it after .
  return 0;
 })
-var id = document.querySelector(".names");
-sortedNames.forEach((i)=>{
-   var newDiv = document.createElement('ol');
-    newDiv.innerHTML = i;
-   id.appendChild(newDiv)
+
+
+var id = document.getElementById("list");
+allNames.forEach((i)=>{
+  var newDiv = document.createElement('li');   // creater an new list that takes all the names
+  id.appendChild(newDiv)  //adds the list to the empty names div
+  newDiv.classList.add("addCSS");
+  newDiv.innerHTML = i;  // prints the names by replacing the text in the empty div.
 })
+
+// I was not anle to figure out how to make just the last names red and bold. 
